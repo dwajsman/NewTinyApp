4b3d97d0689344277ae9a0b77bba45a9182e8aee
Daniel Wajsman <danielwajsman@mac.com>
2021-11-14 21:51:48 -0800

    created URLS NEW ejs

--- express_server.js
+++ express_server.js
@@ -48,6 +48,6 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: href="#" };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
