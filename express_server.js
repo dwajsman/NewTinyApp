00de21d3aa03f946edc66953bd4823e1fead52bd
Daniel Wajsman <danielwajsman@mac.com>
2021-11-14 20:28:33 -0800

    add hello page

--- express_server.js
+++ express_server.js
@@ -20,4 +20,7 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-
+// testing code with 'Hello World'
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
