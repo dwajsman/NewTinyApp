a7187e77b8b4fbee31a212150a15cb57aa554ffe
Daniel Wajsman <danielwajsman@mac.com>
2021-11-14 20:30:46 -0800

    test if request visible for dif pages (isn't!)

--- express_server.js
+++ express_server.js
@@ -24,3 +24,15 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+// test if request visible for dif pages (isn't!)
+app.get("/set", (req, res) => {
+ const a = 1;
+ res.send(`a = ${a}`);
+});
+
+app.get("/fetch", (req, res) => {
+ res.send(`a = ${a}`);
+});
+
+
